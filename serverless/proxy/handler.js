de0c551a9bab9a3832f60a6443adb5c2b0950625
bulabu <soundarajan3@gmail.com>
2023-04-25 20:46:27 -0400

    Basic serverless service with domain name

---
+++ serverless/proxy/handler.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports.submit = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+module.exports.content = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
